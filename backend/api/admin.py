294ccc9ae640cb83c373f7e90551fc20a890d0ca
Akif Fazil GUVEN <akiff.fazill@gmail.com>
2024-05-26 19:37:35 +0300

    updated admin panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from .models import Randevular, Personeller
+from django.contrib.admin import DateFieldListFilter
+from rangefilter.filters import DateRangeFilter
 
 # Register your models here.
 
 
+def onay_durumu_degistir(modeladmin, request, queryset):
+    for randevu in queryset:
+        randevu.onay_durumu = True
+        randevu.save()
+
+    modeladmin.message_user(request, "Secilen randuvular onaylandi")
+
+onay_durumu_degistir.short_description = "Randevulari Onayla"
+
+class RandevularAdmin(admin.ModelAdmin):
+    list_display = ('name_surname', 'phone', 'date', 'note', 'onay_durumu')
+    list_filter = (('date', DateRangeFilter),)
+    ordering = ('date',)
+    actions = [onay_durumu_degistir]
+
 
 admin.site.register(Personeller)
-admin.site.register(Randevular)
+admin.site.register(Randevular, RandevularAdmin)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
 
     'corsheaders',
+    'rangefilter',
 
     'rest_framework',
     'api.apps.ApiConfig',
